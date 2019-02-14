a4b6611cff8a6261ed4ab7e8f2ddc0bdfd5f6f3e
mh-skjelvareid <martin.hansen.skjelvareid@gmail.com>
2019-02-14 13:52:25 +0100

    Included relay control

--- facetracking/ArduinoSerialServoControl/ArduinoSerialServoControl.ino
+++ facetracking/ArduinoSerialServoControl/ArduinoSerialServoControl.ino
@@ -1,4 +1,5 @@
 #include <Servo.h>
+Servo rcRelay;
 Servo panServo;
 Servo tiltServo;
 String inByte;
@@ -7,41 +8,58 @@ int pos;
 /*
    The code receives serial data and adjusts the servo positions. 
    A command to change position comes in parts:
-    - First byte: 'P' or 'T' (indicates pan or tilt)
+    - First byte: 'R','P' or 'T' (indicates relay, pan or tilt)
     - Remaining bytes: Interpreted as ASCII numbers, converted to desired angle
+      (for the relay, an angle below 90 is off, above 90 is on)
 */
 
 // Connect servos
 void setup() {
+  rcRelay.attach(2);      // Relay on pin 2
   panServo.attach(9);     // Pan servo on pin 9
   tiltServo.attach(10);   // Tilt servo on pin 10
-  Serial.begin(115200);     // Baud rate 9600
+  Serial.begin(115200);   // Baud rate 115200
 }
 
-
+// Main loop
 void loop()
 {
   int val =  0;           /* byte value read from serial */
 
+  rcRelay.write(180);
+
+  
   if (Serial.available() >0) {
     // Read first byte (indicates which servo to adjust)
     val = Serial.read();
 
-    // Adjust position if command 'P' or 'T'
-    if (val=='P' || val=='T') 
+    // Adjust position if command 'R', 'P' or 'T'
+    if (val == 'R' || val=='P' || val=='T') 
     {
       inByte = Serial.readStringUntil('\n'); // read data until newline
       pos = inByte.toInt();   // change datatype from string to integer
       
-      if(val=='P') 
-      {
+      if(val=='R'){     // RC relay
+        
+        if(pos==0){
+          rcRelay.write(0);
+          Serial.println("Relay OFF");
+        }
+        
+        if(pos==1){
+          rcRelay.write(180);    // Maximum servo angle
+          Serial.println("Relay ON");
+        }
+                
+      }
+      
+      if(val=='P'){    // Pan servo
         panServo.write(pos);
         Serial.print("Pan:  ");
         Serial.println(inByte);
-      } // end if(val=='P') 
+      } 
       
-      else 
-      {
+      if(val=='T'){      // Tilt servo
         tiltServo.write(pos);
         Serial.print("Tilt: ");
         Serial.println(inByte);
